36a3e6b3302b336300ef7abab61fef49a67fc387
Oleksandr Bielodubrovskyi <alexrns3@gmail.com>
2021-06-08 00:46:50 +0300

    Try filtering

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,12 @@
 module ProductsHelper
-  def brands_list(products)
-    products.map(&:brand).uniq
-  end
+  # def brands_list(products)
+  #   products.map(&:brand).uniq
+  # end
   def checked?(key, value)
     params[key].present? && params[key].include(value)
+  end
+
+  def uniq_brand
+    Product.all.uniq { |p| p.brand }
   end
 end
