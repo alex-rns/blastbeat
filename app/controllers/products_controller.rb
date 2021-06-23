6f3fbf2c517a60397c4dadf7a53120d0c9e34067
“OlekasndrBielodubrovskyi” <alexrns3@gmail.com>
2021-06-23 10:18:38 +0300

    Added link to product show, devided in the partial some blocks,  added to product controller some action for full CDUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
+  # TODO: can I create before action with "set_response" (respond_with(@products)) ?
+
   def index
     @products = FilterProductsQuery.new(Product.all, params).call
   end
 
-  def show
-    @product = Product.find(params[:id])
-  end
+  def show; end
 
   def new
     @product = Product.new
@@ -16,14 +18,26 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(product_params)
-    respond_with(@product, location: home_path)
+    respond_with(@product)
   end
 
   def edit; end
 
-  def update; end
+  def update
+    @product.update(product_params)
+    respond_with(@product)
+  end
 
-  def destroy; end
+  def destroy
+    @product.destroy
+    respond_with(@products, location: home_path)
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:title, :description, :price, :category, :brand, :manufacturer, :year)
