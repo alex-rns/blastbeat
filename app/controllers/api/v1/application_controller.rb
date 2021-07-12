1e698c7b61bd7550c9f3a785781b6edd45d711b1
“OlekasndrBielodubrovskyi” <alexrns3@gmail.com>
2021-07-12 14:19:57 +0300

    Devided Api and main controllers

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -2,10 +2,29 @@
 
 require 'application_responder'
 
-# :nodoc:
 module Api
   module V1
+    # :nodoc:
     class ApplicationController < ApplicationController
+      acts_as_token_authentication_handler_for User, fallback: :none
+
+      before_action :authenticate_user!
+      self.responder = ApplicationResponder
+
+      respond_to :json, :html
+
+      def current_user
+        @current_user ||= @user
+      end
+
+      private
+
+      def authenticate_user!
+        unless current_user
+          render json: { error: I18n.t('devise.failure.unauthenticated') },
+                 status: :forbidden
+        end
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,9 @@ require 'application_responder'
 
 # :nodoc:
 class ApplicationController < ActionController::Base
-  acts_as_token_authentication_handler_for User, fallback: :none
   before_action :set_user
   before_action :authenticate_user!
   self.responder = ApplicationResponder
-
-  respond_to :json, :html
 
   protect_from_forgery with: :null_session
 
@@ -17,19 +14,6 @@ class ApplicationController < ActionController::Base
     if request.headers['Authorization'].present?
       token = request.headers['Authorization'].split(' ')[1]
       @user = User.find_by(authentication_token: token)
-    end
-  end
-
-  def current_user
-    @current_user ||= @user
-  end
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      render json: { error: I18n.t('devise.failure.unauthenticated') },
-             status: :forbidden
     end
   end
 end
