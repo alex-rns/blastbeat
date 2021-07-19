f282bd7f13a70fac47bd506356033ea4405c28d8
“OlekasndrBielodubrovskyi” <alexrns3@gmail.com>
2021-07-19 14:44:05 +0300

    Refactored

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,7 +5,6 @@ module Api
     # :nodoc:
     class CommentsController < ApplicationController
       def index
-        Comment.all
         render json: Comment.all, message: 'Comment list', status: :ok
       end
     end
